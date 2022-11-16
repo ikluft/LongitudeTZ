18ace17784552de38427cd0b0dbdd136f862147f
Ian Kluft <ian.kluft+github@gmail.com>
2022-11-15 23:23:53 -0800

    add to perl TimeZone::Solar (work in progress)

--- src/perl/bin/gen-ltz.pl
+++ src/perl/bin/gen-ltz.pl
@@ -1,12 +1,21 @@
 #!/usr/bin/perl 
-# generate longitude-based solar timezone info files
-# gen-ltz.pl by Ian Kluft
+# ABSTRACT: generate longitude-based solar timezone info files
+# PODNAME: gen-ltz.pl
 # CREATED: 09/14/2020
-# Perl implementation
-use strict;
-use warnings;
+# part of Perl implementation of solar timezones library
+#
+# Copyright © 2020-2022 Ian Kluft. This program is free software; you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License Version 3. See  https://www.gnu.org/licenses/gpl-3.0-standalone.html
+
+# pragmas to silence some warnings from Perl::Critic
+## no critic (Modules::RequireExplicitPackage)
+# This solves a catch-22 where parts of Perl::Critic want both package and use-strict to be first
+use Modern::Perl qw(2017);
+## use critic (Modules::RequireExplicitPackage)
+
+use v5.26.0;    # 2017 Perl or newer
 use utf8;
-use v5.26.0;    # 2018 Perl or newer
 use autodie;
 use feature qw(say);
 use Carp qw(croak);
--- src/perl/lib/TimeZone/Solar.pm
+++ src/perl/lib/TimeZone/Solar.pm
@@ -1,10 +1,46 @@
 # TimeZone::Solar
-# copyright © by Ian Kluft
-
-use strict;
-use warnings;
-package TimeZone::Solar;
-
 # ABSTRACT: local solar timezone lookup and utilities
+# part of Perl implementation of solar timezones library
+#
+# Copyright © 2020-2022 Ian Kluft. This program is free software; you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License Version 3. See  https://www.gnu.org/licenses/gpl-3.0-standalone.html
+
+# pragmas to silence some warnings from Perl::Critic
+## no critic (Modules::RequireExplicitPackage)
+# This solves a catch-22 where parts of Perl::Critic want both package and use-strict to be first
+use Modern::Perl qw(2017);
+## use critic (Modules::RequireExplicitPackage)
+
+package TimeZone::Solar;
+use utf8;
+use autodie;
+use Carp qw(croak);
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+  use TimeZone::Solar;
+
+  my $solar_tz1 = TimeZone::Solar->new(lat => $latiude, lon => $longitude);
+  my $solar_tz2 = TimeZone::Solar->new(lon => $longitude); # assumes latitude between 80N and 80S
+  my $tz_name = $solar_tz1->name();
+  my $tz_offset = $solar_tz1->offset(); # minutes difference from GMT
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS AND METHODS
+
+=head1 LICENSE
+
+TimeZone::Solar is Open Source software licensed under the GNU General Public License Version 3.
+See L<https://www.gnu.org/licenses/gpl-3.0-standalone.html>.
+
+=head1 BUGS AND LIMITATIONS
+
+=cut
