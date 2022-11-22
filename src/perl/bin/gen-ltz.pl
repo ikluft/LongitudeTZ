3fdb799dcced7051ec08b66593c22fd0c28e7d38
Ian Kluft <ian.kluft+github@gmail.com>
2022-11-22 01:03:44 -0800

    cleanup via perltidy

--- src/perl/bin/gen-ltz.pl
+++ src/perl/bin/gen-ltz.pl
@@ -17,13 +17,12 @@ use Modern::Perl qw(2018);
 use utf8;
 use autodie;
 use feature qw(say);
-use Carp qw(croak);
+use Carp    qw(croak);
 
 # generate standard 1-hour-wide (15 degrees longitude) time zones
 # input parameter: integer hours from GMT in the range
 # These correspond to the GMT+x/GMT-x time zones, except with boundaries defined by longitude lines.
-sub gen_hour_tz
-{
+sub gen_hour_tz {
     my $hour = int(shift);
     if ( $hour < -12 or $hour > 12 ) {
         croak "hour parameter must be -12 to +12 inclusive";
@@ -35,13 +34,14 @@ sub gen_hour_tz
     my $offset_min = 0;
 
     # generate strings from time zone parameters
-    my $zone_abbrev = sprintf( "%s%s%02d",  "Solar",  $sign, $offset_hr );
+    my $zone_abbrev = sprintf( "%s%s%02d",  "Solar", $sign, $offset_hr );
     my $zone_name   = sprintf( "%s/%s",     "Solar", $zone_abbrev );
     my $offset_str  = sprintf( "%s%d:%02d", $sign,   $offset_hr, $offset_min );
 
     # output time zone data
     say "# Solar Time by hourly increment: $sign $offset_hr";
-    say "# " . join( "\t", qw(Zone NAME ), "", qw(STDOFF RULES FORMAT [UNTIL]) );
+    say "# "
+      . join( "\t", qw(Zone NAME ), "", qw(STDOFF RULES FORMAT [UNTIL]) );
     say join( "\t", "Zone", $zone_name, $offset_str, "-", $zone_abbrev );
     say "";
     return;
@@ -51,21 +51,20 @@ sub gen_hour_tz
 # input parameter: integer degrees of longitude in the range 180 to -180, Solar Time Zone centered on the meridian,
 # including one half degree either side of the meridian. Each time zone is named for its 1-degree-wide range.
 # The exception is at the Solar Date Line, where +12 and -12 time zones are one half degree wide.
-sub gen_lon_tz
-{
+sub gen_lon_tz {
     my $deg = int(shift);
     if ( $deg < -180 or $deg > 180 ) {
         croak "deg parameter must be -180 to +180 inclusive";
     }
 
-    # use integer degrees to compute time zone parameters: longitude, east/west sign and minutes offset
-    # $deg>=0: positive degrees (east longitude), straightforward assignments of data
-    # $deg<0: negative degrees (west longitude)
+# use integer degrees to compute time zone parameters: longitude, east/west sign and minutes offset
+# $deg>=0: positive degrees (east longitude), straightforward assignments of data
+# $deg<0: negative degrees (west longitude)
     my $lon  = abs($deg);
-    my $ew   = ($deg >= 0) ? "E" : "W";
-    my $sign = ($deg >= 0) ? "" : "-";
+    my $ew   = ( $deg >= 0 ) ? "E" : "W";
+    my $sign = ( $deg >= 0 ) ? ""  : "-";
 
-    # derive time zone parameters from 4 minutes of offset for each degree of longitude
+# derive time zone parameters from 4 minutes of offset for each degree of longitude
     my $offset     = 4 * abs($deg);
     my $offset_hr  = int( abs($offset) / 60 );
     my $offset_min = abs($offset) % 60;
@@ -77,7 +76,8 @@ sub gen_lon_tz
 
     # output time zone data
     say "# Solar Time by degree of longitude: $lon $ew";
-    say "# " . join( "\t", qw(Zone NAME ), "", qw(STDOFF RULES FORMAT [UNTIL]) );
+    say "# "
+      . join( "\t", qw(Zone NAME ), "", qw(STDOFF RULES FORMAT [UNTIL]) );
     say join( "\t", "Zone", $zone_name, $offset_str, "-", $zone_abbrev );
     say "";
     return;
@@ -89,13 +89,13 @@ sub gen_lon_tz
 
 # generate solar time zones in increments of 15 degrees of longitude (STHxxE/STHxxW)
 # standard 1-hour-wide time zones
-foreach my $hour (-12..12) {
+foreach my $hour ( -12 .. 12 ) {
     gen_hour_tz($hour);
 }
 
 # generate solar time zones in incrememnts of 4 minutes / 1 degree of longitude (STLxxxE/STxxxW)
 # hyperlocal 4-minute-wide time zones for conversion to/from niche uses of local solar time
-foreach my $deg (-180..180) {
+foreach my $deg ( -180 .. 180 ) {
     gen_lon_tz($deg);
 }
 
--- src/perl/lib/TimeZone/Solar.pm
+++ src/perl/lib/TimeZone/Solar.pm
@@ -19,32 +19,37 @@ use Carp qw(croak);
 use Readonly;
 
 # constants
-Readonly::Scalar my $PRECISION_DIGITS => 6;                                 # max decimal digits of precision
-Readonly::Scalar my $PRECISION_FP => (10 ** -$PRECISION_DIGITS) / 2.0;      # 1/2 width of floating point equality
-Readonly::Scalar my $MAX_DEGREES => 360;                                    # maximum degrees = 360
-Readonly::Scalar my $MAX_LONGITUDE_INT => $MAX_DEGREES / 2;                 # min/max longitude in integer = 180
-Readonly::Scalar my $MAX_LONGITUDE_FP => $MAX_DEGREES / 2.0;                # min/max longitude in float = 180.0
-Readonly::Scalar my $MAX_LATITUDE_FP => $MAX_DEGREES / 4.0;                 # min/max latitude in float = 90.0
-Readonly::Scalar my $POLAR_UTC_AREA => 10;                                  # latitude degrees around poles to use UTC
-Readonly::Scalar my $LIMIT_LATITUDE => $MAX_LATITUDE_FP - $POLAR_UTC_AREA;  # max latitude for solar time zones
-Readonly::Scalar my $MINUTES_PER_DEGREE_LON => 4;                           # minutes per degree longitude
+Readonly::Scalar my $PRECISION_DIGITS => 6;    # max decimal digits of precision
+Readonly::Scalar my $PRECISION_FP => ( 10**-$PRECISION_DIGITS ) /
+  2.0;    # 1/2 width of floating point equality
+Readonly::Scalar my $MAX_DEGREES => 360;    # maximum degrees = 360
+Readonly::Scalar my $MAX_LONGITUDE_INT => $MAX_DEGREES /
+  2;                                        # min/max longitude in integer = 180
+Readonly::Scalar my $MAX_LONGITUDE_FP => $MAX_DEGREES /
+  2.0;                                      # min/max longitude in float = 180.0
+Readonly::Scalar my $MAX_LATITUDE_FP => $MAX_DEGREES /
+  4.0;                                      # min/max latitude in float = 90.0
+Readonly::Scalar my $POLAR_UTC_AREA =>
+  10;    # latitude degrees around poles to use UTC
+Readonly::Scalar my $LIMIT_LATITUDE => $MAX_LATITUDE_FP -
+  $POLAR_UTC_AREA;    # max latitude for solar time zones
+Readonly::Scalar my $MINUTES_PER_DEGREE_LON => 4; # minutes per degree longitude
 Readonly::Hash my %constants => (
-    PRECISION_DIGITS => $PRECISION_DIGITS,
-    PRECISION_FP => $PRECISION_FP,
-    MAX_DEGREES => $MAX_DEGREES,
-    MAX_LONGITUDE_INT => $MAX_LONGITUDE_INT,
-    MAX_LONGITUDE_FP => $MAX_LONGITUDE_FP,
-    MAX_LATITUDE_FP => $MAX_LATITUDE_FP,
-    POLAR_UTC_AREA => $POLAR_UTC_AREA,
-    LIMIT_LATITUDE => $LIMIT_LATITUDE,
+    PRECISION_DIGITS       => $PRECISION_DIGITS,
+    PRECISION_FP           => $PRECISION_FP,
+    MAX_DEGREES            => $MAX_DEGREES,
+    MAX_LONGITUDE_INT      => $MAX_LONGITUDE_INT,
+    MAX_LONGITUDE_FP       => $MAX_LONGITUDE_FP,
+    MAX_LATITUDE_FP        => $MAX_LATITUDE_FP,
+    POLAR_UTC_AREA         => $POLAR_UTC_AREA,
+    LIMIT_LATITUDE         => $LIMIT_LATITUDE,
     MINUTES_PER_DEGREE_LON => $MINUTES_PER_DEGREE_LON,
 );
 
 # access constants - for use by tests
 # throws exception if requested contant name doesn't exist
 ## no critic ( Subroutines::ProhibitUnusedPrivateSubroutines )
-sub _get_const
-{
+sub _get_const {
     my ( $class, $name ) = @_;
     if ( not exists $constants{$name} ) {
         croak "non-existent constant requested: $name";
@@ -54,15 +59,16 @@ sub _get_const
 ## critic ( Subroutines::ProhibitUnusedPrivateSubroutines )
 
 # return TimeZone::Solar (or subclass) version number
-sub version
-{
+sub version {
     my $class = shift;
 
     if ( not defined $class ) {
         throw_incompatible_class("invalid version() call on undefined value");
     }
     if ( not $class->isa(__PACKAGE__) ) {
-        throw_incompatible_class("invalid version() call for '$class': not in ".__PACKAGE__." hierarchy");
+        throw_incompatible_class( "invalid version() call for '$class': not in "
+              . __PACKAGE__
+              . " hierarchy" );
     }
     {
         ## no critic (TestingAndDebugging::ProhibitNoStrict)
@@ -75,39 +81,42 @@ sub version
 }
 
 # check latitude data and initialize special case for polar regions - internal method called by init()
-sub _init_latitude
-{
-    my $self = shift;
+sub _init_latitude {
+    my $self  = shift;
     my $class = ref $self;
 
     # safety check on latitude
-    if ( $self->{latitude} > $MAX_LATITUDE_FP or $self->{latitude} < -$MAX_LATITUDE_FP ) {
+    if (   $self->{latitude} > $MAX_LATITUDE_FP
+        or $self->{latitude} < -$MAX_LATITUDE_FP )
+    {
         croak "$class: latitude when provided must be in range -90..+90";
     }
 
     # special case: use Solar+00 (equal to UTC) within 10° latitude of poles
-    if ( $self->{latitude} >= $LIMIT_LATITUDE or $self->{latitude} <= -$LIMIT_LATITUDE ) {
-        $self->name( "Solar+00" );
-        $self->offset( 0 );
+    if (   $self->{latitude} >= $LIMIT_LATITUDE
+        or $self->{latitude} <= -$LIMIT_LATITUDE )
+    {
+        $self->name("Solar+00");
+        $self->offset(0);
     }
     return;
 }
 
 # initialize - called by new()
-sub init
-{
-    my $self = shift;
+sub init {
+    my $self  = shift;
     my $class = ref $self;
-    if ( not exists $self->{longitude}) {
+    if ( not exists $self->{longitude} ) {
         croak "$class: longitude parameter missing";
     }
 
     # if latitude is provided, use UTC within 10° latitude of poles
-    if ( exists $self->{latitude}) {
+    if ( exists $self->{latitude} ) {
+
         # check latitude data and initialize special case for polar regions
         $self->_init_latitude();
 
-        # return if initialized, otherwise fall through to set time zone from longitude as usual
+# return if initialized, otherwise fall through to set time zone from longitude as usual
         return if exists $self->{name} and exists $self->{offset};
     }
 
@@ -116,55 +125,65 @@ sub init
     #
 
     # safety check on longitude
-    if ( $self->{longitude} > $MAX_LONGITUDE_FP or $self->{longitude} < -$MAX_LONGITUDE_FP ) {
+    if (   $self->{longitude} > $MAX_LONGITUDE_FP
+        or $self->{longitude} < -$MAX_LONGITUDE_FP )
+    {
         croak "$class: longitude must be in range -180 to +180";
     }
 
-    # floating point equality test for solar date line: flip sign positive if within precision margin of the line
+# floating point equality test for solar date line: flip sign positive if within precision margin of the line
     if ( $self->{longitude} < -$MAX_LONGITUDE_FP + $PRECISION_FP ) {
         $self->{longitude} = -$self->{longitude};
     }
 
-    # set flag for longitude time zones: 0 = hourly 1-hour/15-degree zones, 1 = longitude 4-minute/1-degree zones
-    # defaults to hourly time zone ($use_lon_tz=0)
-    my $use_lon_tz = ( exists $self->{use_lon_tz} and $self->{use_lon_tz});
-    my $tz_degree_width = $use_lon_tz ? 1 : 15; # 1 for longitude-based tz, 15 for hour-based tz
-    my $tz_max = $MAX_LONGITUDE_INT / $tz_degree_width; # ±180 for longitude-based tz, ±12 for hour-based tz
-    my $tz_type = $use_lon_tz ? "Lon" : "Solar";
-    my $tz_digits = $use_lon_tz ? 3 : 2;
+# set flag for longitude time zones: 0 = hourly 1-hour/15-degree zones, 1 = longitude 4-minute/1-degree zones
+# defaults to hourly time zone ($use_lon_tz=0)
+    my $use_lon_tz = ( exists $self->{use_lon_tz} and $self->{use_lon_tz} );
+    my $tz_degree_width =
+      $use_lon_tz ? 1 : 15;    # 1 for longitude-based tz, 15 for hour-based tz
+    my $tz_max = $MAX_LONGITUDE_INT /
+      $tz_degree_width;    # ±180 for longitude-based tz, ±12 for hour-based tz
+    my $tz_type   = $use_lon_tz ? "Lon" : "Solar";
+    my $tz_digits = $use_lon_tz ? 3     : 2;
 
     # handle special case of tz centered on Prime Meridian (0° longitude)
-    if ( $self->{longitude} > -$tz_degree_width/2.0 and $self->{longitude} < $tz_degree_width/2.0 ) {
+    if (    $self->{longitude} > -$tz_degree_width / 2.0
+        and $self->{longitude} < $tz_degree_width / 2.0 )
+    {
         my $tz_name = sprintf "%s%s%0*d", $tz_type, "+", $tz_digits, 0;
-        $self->name( $tz_name );
-        $self->offset( 0 );
+        $self->name($tz_name);
+        $self->offset(0);
         return;
     }
 
-    # handle special case of half-wide tz at positive side of solar date line (180° longitude)
-    if ( $self->{longitude} >= $tz_max - $tz_degree_width/2 - $PRECISION_FP ) {
+# handle special case of half-wide tz at positive side of solar date line (180° longitude)
+    if ( $self->{longitude} >= $tz_max - $tz_degree_width / 2 - $PRECISION_FP )
+    {
         my $tz_name = sprintf "%s%s%0*d", $tz_type, "+", $tz_digits,
-            $MAX_LONGITUDE_INT / $tz_degree_width;
-        $self->name( $tz_name );
-        $self->offset( 720 );
+          $MAX_LONGITUDE_INT / $tz_degree_width;
+        $self->name($tz_name);
+        $self->offset(720);
         return;
     }
 
-    # handle special case of half-wide tz at negativ< side of solar date line (180° longitude)
-    if ( $self->{longitude} <= -$tz_max + $tz_degree_width/2 + $PRECISION_FP ) {
+# handle special case of half-wide tz at negativ< side of solar date line (180° longitude)
+    if ( $self->{longitude} <= -$tz_max + $tz_degree_width / 2 + $PRECISION_FP )
+    {
         my $tz_name = sprintf "%s%s%0*d", $tz_type, "-", $tz_digits,
-            $MAX_LONGITUDE_INT / $tz_degree_width;
-        $self->name( $tz_name );
-        $self->offset( -720 );
+          $MAX_LONGITUDE_INT / $tz_degree_width;
+        $self->name($tz_name);
+        $self->offset(-720);
         return;
     }
 
     # handle other times zones
-    my $tz_name = sprintf "%s%s%0*d", $tz_type, $self->{longitude} >= 0 ? "+" : "-",
-        $tz_digits, int( abs( $self->{longitude} / $tz_degree_width ) - 0.5 );
-    my $offset = int( $self->{longitude} / $tz_degree_width - 0.5 ) * ( $MINUTES_PER_DEGREE_LON * $tz_degree_width);
-    $self->name( $tz_name );
-    $self->offset( $offset );
+    my $tz_name = sprintf "%s%s%0*d", $tz_type,
+      $self->{longitude} >= 0 ? "+" : "-",
+      $tz_digits, int( abs( $self->{longitude} / $tz_degree_width ) - 0.5 );
+    my $offset = int( $self->{longitude} / $tz_degree_width - 0.5 ) *
+      ( $MINUTES_PER_DEGREE_LON * $tz_degree_width );
+    $self->name($tz_name);
+    $self->offset($offset);
     return;
 }
 
@@ -174,23 +193,22 @@ sub new {
     my $class = ref($in_class) || $in_class;
 
     # safety check
-    if ( not $class->isa(__PACKAGE__)) {
-        croak __PACKAGE__."->new() prohibited for unrelated class $class";
+    if ( not $class->isa(__PACKAGE__) ) {
+        croak __PACKAGE__ . "->new() prohibited for unrelated class $class";
     }
 
     # instantiate object with @args data
-    my $self = bless { @args }, $class;
+    my $self = bless {@args}, $class;
 
     # use init() method of proper class, possibly a derived class
-    if ( my $init_func = $self->can("init")) {
-        $init_func->( $self );
+    if ( my $init_func = $self->can("init") ) {
+        $init_func->($self);
     }
     return $self;
 }
 
 # accessor methods
-sub name
-{
+sub name {
     my @args = @_;
     my $self = $args[0];
     if ( scalar @args > 1 ) {
@@ -198,8 +216,8 @@ sub name
     }
     return $self->{name};
 }
-sub offset
-{
+
+sub offset {
     my @args = @_;
     my $self = $args[0];
     if ( scalar @args > 1 ) {
