74235ea20580fbe9be7324d4c83abeb1d29c4603
Ian Kluft <ian.kluft+github@gmail.com>
2022-12-26 01:03:53 -0800

    Python troubleshooting: simplify by abandoning singleton model; misc fixes & cleanups

--- src/python/timezone_solar/timezone_solar.py
+++ src/python/timezone_solar/timezone_solar.py
@@ -4,9 +4,6 @@ from datetime import tzinfo, timedelta
 import re
 from timezone_solar.tzsconst import TZSConst
 
-# instances of each time zone's singleton object
-_instances = {}
-
 class TimeZoneSolar(tzinfo):
     """local solar timezone"""
 
@@ -20,7 +17,7 @@ class TimeZoneSolar(tzinfo):
     #   use_lon_tz: true=use longitude-based time zones, false=use hour-based time zones
     #   sign: +1 = positive/zero, -1 = negative
     @staticmethod
-    def _tz_name(**params):
+    def _tz_name(**params) -> str:
         prefix = "Lon" if params["use_lon_tz"] else ("East" if params["sign"] > 0 else "West" )
         suffix = "" if not  params["use_lon_tz"] else ("E" if params["sign"] > 0 else "W")
         tz_degree_width = 1 if params["use_lon_tz"] else 15
@@ -34,7 +31,7 @@ class TimeZoneSolar(tzinfo):
 
     # get timezone parameters (name and minutes offset) - called by __new__()
     @classmethod
-    def _tz_params(cls, tz_params):
+    def _tz_params(cls, tz_params) -> dict:
         if "longitude" not in tz_params:
             raise Exception( "_tz_params: longitude parameter missing" )
 
@@ -69,13 +66,11 @@ class TimeZoneSolar(tzinfo):
             tz_name = cls._tz_name(use_lon_tz=use_lon_tz, sign=1, \
                 longitude = const.max_longitude_int)
             tz_params["short_name"] = tz_name
-            tz_params["name"] = f"Solar/{tz_name}"
             tz_params["offset_min"] = 720
 
         elif longitude <= -max_longitude + tz_degree_width / 2.0 + const.precision_fp:
             tz_name = cls._tz_name(use_lon_tz=use_lon_tz, sign=-1, longitude=max_longitude)
             tz_params["short_name"] = tz_name
-            tz_params["name"] = f"Solar/{tz_name}"
             tz_params["offset_min"] = -720
 
         else:
@@ -83,40 +78,20 @@ class TimeZoneSolar(tzinfo):
             sign = 1 \
                 if longitude > -tz_degree_width / 2.0 + const.precision_fp \
                 else -1
-            offset = sign * tz_int * const.minutes_per_degree_lon * tz_degree_width
+            offset_min = sign * tz_int * const.minutes_per_degree_lon * tz_degree_width
             tz_name = cls._tz_name(use_lon_tz=use_lon_tz, sign=sign, longitude=tz_int)
             tz_params["short_name"] = tz_name
-            tz_params["name"] = f"Solar/{tz_name}"
-            tz_params["offset_min"] = offset
-
+            tz_params["offset_min"] = offset_min
+        tz_params["name"] = f"Solar/{tz_params['short_name']}"
         return tz_params
 
-    # fetch or create a timezone instance for __new__
-    @classmethod
-    def _tz_instance(cls, params):
-        # consistency checks
-        const = TZSConst()
-        short_name = params["short_name"]
-        if short_name is None:
-            raise Exception( "_tz_instance: short_name parameter missing" )
-        if not re.fullmatch( const.tzsolar_zone_re, short_name):
-            raise Exception( "_tz_instance: short_name parameter is not a solar time zone name" )
+    # create a new instance
+    def __new__(cls, **kwargs):
+        return super().__new__(cls, kwargs)
 
-        # look up class instance, return it if found
-        if short_name in _instances:
-            # forward lat/lon parameters to existing instance, for testing & troubleshooting
-            _instances[short_name].update_lon_lat(params)
-            return _instances[short_name]
-
-        # make and save the singleton instance for that short_name class
-        obj = super().__new__(cls, params)
-        _instances[short_name] = obj
-        return obj
-
-    # return a singleton instance for the requested time zone
-    # create a new instance only if it didn't already exist
-    def __new__(cls, **params):
-        return cls._tz_instance(cls._tz_params(**params))
+    # initialize instance
+    def __init__(self, **kwargs):
+        self.__dict__.update(self.__class__._tz_params(kwargs))
 
     #
     # attribute access methods
@@ -129,21 +104,24 @@ class TimeZoneSolar(tzinfo):
         """
         for key in ["longitude", "latitude"]:
             if key in params:
-                self.setattr(key, params[key])
+                setattr(self, key, params[key])
             else:
-                self.delattr(key)
+                try:
+                    delattr(self, key)
+                except AttributeError:
+                    pass # no problem if item to be deleted didn't exist
 
     # get UTC offset
     # implementation of datetime.tzinfo interface
-    def utcoffset(self, dt):
+    def utcoffset(self, dt) -> timedelta:
         """
         returns a timedelta of the offset from UTC
         """
-        return timedelta(minutes = self["offset_min"])
+        return timedelta(minutes = self.offset_min)
 
     # get DST flag (always false for solar time zones)
     # implementation of datetime.tzinfo interface
-    def dst(self, dt):
+    def dst(self, dt) -> bool:
         """
         returns Daylight Saving Time flag, always false because solar time zones don't use DST
         """
@@ -151,8 +129,8 @@ class TimeZoneSolar(tzinfo):
 
     # get time zone name string
     # implementation of datetime.tzinfo interface
-    def tzname(self, dt):
+    def tzname(self, dt) -> str:
         """
         returns short name of time zone
         """
-        return self["name"]
+        return self.name
