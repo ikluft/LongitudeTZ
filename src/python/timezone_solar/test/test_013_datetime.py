4df4c492f3f9e1ed9063349371a7889f11b8d5a5
Ian Kluft <ian.kluft+github@gmail.com>
2023-01-01 23:59:04 -0800

    Python: fix flake8 warnings

--- src/python/timezone_solar/test/test_013_datetime.py
+++ src/python/timezone_solar/test/test_013_datetime.py
@@ -2,16 +2,19 @@
 """unit tests of timezone_solar package integration with datetime.tzinfo system library"""
 
 import unittest
-from datetime import tzinfo
-from timezone_solar import TimeZoneSolar
+# from datetime import tzinfo
+# from timezone_solar import TimeZoneSolar
+
 
 class TestDateTime(unittest.TestCase):
     """unit tests of timezone_solar package integration with datetime.tzinfo system library"""
 
     @classmethod
     def generate_tests(cls):
+        """generate test functions for integration with datetime.tzinfo system library"""
         testnum = 0
-        # TODO generate tests here
+        # generate tests here
+        testnum += 1
 
 
 if __name__ == "__main__":
