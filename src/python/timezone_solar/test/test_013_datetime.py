a83f250e302fb1f73d75a88976090a71f03e3729
Ian Kluft <ian.kluft+github@gmail.com>
2023-01-03 02:21:44 -0800

    Python: more datetime tests, +484 test cases (total 2758)

--- src/python/timezone_solar/test/test_013_datetime.py
+++ src/python/timezone_solar/test/test_013_datetime.py
@@ -87,6 +87,61 @@ class TestDateTime(unittest.TestCase, LongitudeUtils):
         return check
 
     @classmethod
+    def make_dst_check(cls, testnum, longitude, use_lon_tz) -> callable:
+        """generate test case function for datetime DST delta, which is always zero"""
+        expected_dst = timedelta(0)
+        description = (
+            f"test {PROGNUM:03}-{testnum:03}: longitude={longitude} use_lon_tz={use_lon_tz} "
+            + "→ dst=0"
+        )
+
+        def check(self):
+            tz_test = TimeZoneSolar(
+                longitude=longitude,
+                use_lon_tz=use_lon_tz,
+            )
+            dt_test = datetime(
+                TEST_TIME["year"],
+                TEST_TIME["month"],
+                TEST_TIME["day"],
+                TEST_TIME["hour"],
+                TEST_TIME["minute"],
+                tzinfo=tz_test,
+            )
+            test_dst = dt_test.dst()
+            self.assertEqual(test_dst, expected_dst)
+
+        check.__doc__ = description
+        return check
+
+    @classmethod
+    def make_astimezone_check(cls, testnum, longitude, use_lon_tz) -> callable:
+        """generate test case function for datetime astimezone method"""
+        description = (
+            f"test {PROGNUM:03}-{testnum:03}: longitude={longitude} use_lon_tz={use_lon_tz} "
+            + "→ astimezone match"
+        )
+
+        def check(self):
+            tz_test = TimeZoneSolar(
+                longitude=longitude,
+                use_lon_tz=use_lon_tz,
+            )
+            dt_utc = datetime(
+                TEST_TIME["year"],
+                TEST_TIME["month"],
+                TEST_TIME["day"],
+                TEST_TIME["hour"],
+                TEST_TIME["minute"],
+                tzinfo=timezone.utc,
+            )
+            dt_test = dt_utc.astimezone(tz_test)
+            self.assertEqual(dt_test, dt_utc)
+
+        check.__doc__ = description
+        return check
+
+    @classmethod
     def generate_tests(cls):
         """generate test functions for integration with datetime.tzinfo system library"""
         testnum = 0
@@ -96,14 +151,42 @@ class TestDateTime(unittest.TestCase, LongitudeUtils):
                 # generate tests for datetime tests from timezone_solar object
                 tz_type = "deg" if use_lon_tz else "hour"
                 base_func_name = (
-                    f"test_{PROGNUM:03}_{testnum:03}_lon_{lon_str}_{tz_type}_offset"
+                    f"test_{PROGNUM:03}_{testnum:03}_lon_{lon_str}_{tz_type}"
                 )
-                print(f"generating test {PROGNUM:03}-{testnum:03} as {base_func_name}_offset...")
-                check_offset_func = cls.make_offset_check(testnum, longitude, use_lon_tz)
+
+                # test getting UTC offset back from datetime
+                print(
+                    f"generating test {PROGNUM:03}-{testnum:03} as {base_func_name}_offset..."
+                )
+                check_offset_func = cls.make_offset_check(
+                    testnum, longitude, use_lon_tz
+                )
                 setattr(cls, base_func_name + "_offset", check_offset_func)
-                print(f"generating test {PROGNUM:03}-{testnum:03} as {base_func_name}_name...")
+
+                # test getting timezone name back from datetime
+                print(
+                    f"generating test {PROGNUM:03}-{testnum:03} as {base_func_name}_name..."
+                )
                 check_name_func = cls.make_name_check(testnum, longitude, use_lon_tz)
                 setattr(cls, base_func_name + "_name", check_name_func)
+
+                # test getting timezone dst delta back from datetime
+                print(
+                    f"generating test {PROGNUM:03}-{testnum:03} as {base_func_name}_dst..."
+                )
+                check_dst_func = cls.make_dst_check(testnum, longitude, use_lon_tz)
+                setattr(cls, base_func_name + "_dst", check_dst_func)
+
+                # test getting datetime astimezone() equality test between any time zone and UTC
+                print(
+                    f"generating test {PROGNUM:03}-{testnum:03} as "
+                    + f"{base_func_name}_astimezone..."
+                )
+                check_astimezone_func = cls.make_astimezone_check(
+                    testnum, longitude, use_lon_tz
+                )
+                setattr(cls, base_func_name + "_astimezone", check_astimezone_func)
+
                 testnum += 1
 
 
