d13f7d02fb9ced10c3f8e942c7438bec73462313
Ian Kluft <ian.kluft+github@gmail.com>
2023-01-19 23:51:26 -0800

    Python: more reorg of package directory - tests broken until reorg complete

--- src/python/src/timezone_solar/__init__.py
+++ src/python/src/timezone_solar/__init__.py
@@ -1,3 +1,6 @@
-# pylint: disable=missing-module-docstring
+""" local solar timezone lookup and utilities including datetime compatibility """
 # print(f'timezone_solar __package__=>{__package__} / __name__=>{__name__} / __path__=>{__path__}')
 from .timezone_solar import TimeZoneSolar  # noqa: F401
+
+# set package version
+__version__ = "0.0.1"
