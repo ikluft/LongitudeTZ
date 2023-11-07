e21f43b2a8dfdcaa9392d2daf71316359d02ca4c
Ian Kluft <ko6yq.dev+github@gmail.com>
2023-11-06 23:50:50 -0800

    update regex constants from strings, similar to Python implementation

--- src/cpp/libtzsolar/libtzsolar/libtzsolar.hpp
+++ src/cpp/libtzsolar/libtzsolar/libtzsolar.hpp
@@ -11,9 +11,11 @@ class TZSolar {
     public:
 
     // time zone name regular expressions
-    const std::regex tzsolar_lon_zone_re = std::regex ( "(Lon0[0-9][0-9][EW]) | (Lon1[0-7][0-9][EW]) | (Lon180[EW])" );
-    const std::regex tzsolar_hour_zone_re = std::regex ( "(East|West)(0[0-9] | 1[0-2])" );
-    // const std::regex tzsolar_zone_re = std::regex ( "" );
+    const std::string tzsolar_lon_zone_str = std::string ( "(Lon0[0-9][0-9][EW])|(Lon1[0-7][0-9][EW])|(Lon180[EW])" );
+    const std::string tzsolar_hour_zone_str = std::string ( "(East|West)(0[0-9]|1[0-2])" );
+    const std::regex tzsolar_lon_zone_re = std::regex ( tzsolar_lon_zone_str );
+    const std::regex tzsolar_hour_zone_re = std::regex ( tzsolar_hour_zone_str );
+    const std::regex tzsolar_zone_re = std::regex ( tzsolar_lon_zone_str + "|" + tzsolar_hour_zone_str );
     const int precision_digits = 6;  // max decimal digits of precision
     const double precision_fp = std::pow( 10, -precision_digits ) / 2.0;  // 1/2 width of floating point equality
     const int max_degrees = 360;
