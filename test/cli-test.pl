3c776fb5b56ac9600510d29280112f6178234a33
Ian Kluft <ko6yq.dev+github@gmail.com>
2024-04-14 23:52:29 -0700

    symlink perlcritic.rc & perltidy.rc; tidy up test/cli-test.pl

--- test/cli-test.pl
+++ test/cli-test.pl
@@ -12,21 +12,23 @@ Readonly::Scalar my $TZSOLAR_CLASS_PREFIX => "DateTime::TimeZone::Solar::";
 Readonly::Scalar my $TZSOLAR_LON_ZONE_RE  => qr((Lon0[0-9][0-9][EW]) | (Lon1[0-7][0-9][EW]) | (Lon180[EW]))x;
 Readonly::Scalar my $TZSOLAR_HOUR_ZONE_RE => qr((East|West)(0[0-9] | 1[0-2]))x;
 Readonly::Scalar my $TZSOLAR_ZONE_RE      => qr( $TZSOLAR_LON_ZONE_RE | $TZSOLAR_HOUR_ZONE_RE )x;
-Readonly::Scalar my $total_tests => 14 * 2;
+Readonly::Scalar my $total_tests          => 14 * 2;
 
 # use CLI to get timezone name from longitude tz parameters
 sub cli_tz_name
 {
     my $params_ref = shift;
     my $use_lon_tz = $params_ref->{use_lon_tz} // 0;
-    my $longitude = $params_ref->{longitude};
+    my $longitude  = $params_ref->{longitude};
 
-    if ( $use_lon_tz ) {
+    if ($use_lon_tz) {
+
         # generate longitude-based tz name
-        return sprintf( "Lon%03d%1s", abs(int($longitude)), $longitude < 0 ? "W" : "E",);
+        return sprintf( "Lon%03d%1s", abs( int($longitude) ), $longitude < 0 ? "W" : "E", );
     } else {
+
         # generate hour-based tz name
-        return sprintf( "%4s%02d",  $longitude < 0 ? "West" : "East", abs(int(($longitude + 0.5) / 15)));
+        return sprintf( "%4s%02d", $longitude < 0 ? "West" : "East", abs( int( ( $longitude + 0.5 ) / 15 ) ) );
     }
 }
 
@@ -37,13 +39,14 @@ sub is_valid_name
     say STDERR "debug: testing for valid name: $name";
 
     # run CLI command to generate name and verify against expected valid name
-    my $progpath = $params_ref->{progpath};
-    my $longitude = $params_ref->{longitude};
+    my $progpath   = $params_ref->{progpath};
+    my $longitude  = $params_ref->{longitude};
     my $use_lon_tz = $params_ref->{use_lon_tz} // 0;
-    my $type_str = $use_lon_tz ? "longitude" : "hour";
-    my $output = qx($progpath --longitude=$longitude --type=$type_str --get=short_name);
+    my $type_str   = $use_lon_tz ? "longitude" : "hour";
+    my $output     = qx($progpath --longitude=$longitude --type=$type_str --get=short_name);
     chomp $output;
     my $result = ( $output eq $name );
+
     if ( not $result ) {
         say STDERR "debug: failed to match $output vs $name";
     }
@@ -56,10 +59,10 @@ sub run_validity_test_lon
     my ( $progpath, $lon ) = @_;
 
     foreach my $use_lon_tz ( 0 .. 1 ) {
-        my %params = ( progpath => $progpath, longitude => $lon, use_lon_tz => $use_lon_tz );
+        my %params  = ( progpath => $progpath, longitude => $lon, use_lon_tz => $use_lon_tz );
         my @tznames = cli_tz_name( \%params );
-        foreach my $name ( @tznames ) {
-            ok( is_valid_name( \%params, $name), "verified $name" );
+        foreach my $name (@tznames) {
+            ok( is_valid_name( \%params, $name ), "verified $name" );
         }
     }
     return;
@@ -71,10 +74,10 @@ sub run_validity_tests
     my $progpath = shift;
 
     foreach my $lon1 (qw( -180 -179.75 )) {
-        run_validity_test_lon($progpath, $lon1);
+        run_validity_test_lon( $progpath, $lon1 );
     }
     for ( my $lon2 = -179 ; $lon2 <= 180 ; $lon2 += 30 ) {
-        run_validity_test_lon($progpath, $lon2);
+        run_validity_test_lon( $progpath, $lon2 );
     }
     return;
 }
@@ -84,18 +87,16 @@ sub run_validity_tests
 #
 
 # read CLI parameters to locate program to test
-if ( scalar @ARGV == 0 ){
+if ( scalar @ARGV == 0 ) {
     say STDERR "usage: $0 program-name";
     exit 1;
 }
 my $progpath = $ARGV[0];
 if ( not -f $progpath ) {
-    croak "file does not exist: $progpath"
+    croak "file does not exist: $progpath";
 }
 
 # run tests
 plan tests => $total_tests;
 autoflush STDOUT 1;
-run_validity_tests( $progpath );
-
-
+run_validity_tests($progpath);
