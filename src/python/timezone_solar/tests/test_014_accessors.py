f82d4905a8ead298fda3f44c460ee18dbd32e9fa
Ian Kluft <ko6yq.dev+github@gmail.com>
2024-05-21 23:40:04 -0700

    Python: add tzname tests, +54 unit test cases

--- src/python/timezone_solar/tests/test_014_accessors.py
+++ src/python/timezone_solar/tests/test_014_accessors.py
@@ -17,8 +17,22 @@ TEST_FIXTURE = [
                    "offset_sec": "43200", "is_utc": "0"},
     },
     {
+        # date line +180 degrees longitude, hour-wide tz
+        "params": {"tzname": "East12"},
+        "expect": {"name": "Solar/East12", "short_name": "East12", "long_name": "Solar/East12",
+                   "longitude": "180", "latitude": "", "offset": "+12:00", "offset_min": "720",
+                   "offset_sec": "43200", "is_utc": "0"},
+    },
+    {
         # date line +180 degrees longitude, degree-wide tz
         "params": {"longitude": 180.0, "use_lon_tz": True},
+        "expect": {"name": "Solar/Lon180E", "short_name": "Lon180E", "long_name": "Solar/Lon180E",
+                   "longitude": "180", "latitude": "", "offset": "+12:00", "offset_min": "720",
+                   "offset_sec": "43200", "is_utc": "0"},
+    },
+    {
+        # date line +180 degrees longitude, degree-wide tz
+        "params": {"tzname": "Lon180E"},
         "expect": {"name": "Solar/Lon180E", "short_name": "Lon180E", "long_name": "Solar/Lon180E",
                    "longitude": "180", "latitude": "", "offset": "+12:00", "offset_min": "720",
                    "offset_sec": "43200", "is_utc": "0"},
@@ -45,10 +59,24 @@ TEST_FIXTURE = [
                    "offset_sec": "-28800", "is_utc": "0"},
     },
     {
+        # Portland Int'l Airport PDX: -122.597 longitude, +45.589 latititude, hour-wide tz
+        "params": {"tzname": "West08"},
+        "expect": {"name": "Solar/West08", "short_name": "West08", "long_name": "Solar/West08",
+                   "longitude": "-120", "latitude": "", "offset": "-08:00", "offset_min": "-480",
+                   "offset_sec": "-28800", "is_utc": "0"},
+    },
+    {
         # Portland Int'l Airport PDX: -122.597 longitude, +45.589 latititude, degree-wide tz
         "params": {"longitude": -122.597, "use_lon_tz": True, "latitude": 45.589},
         "expect": {"name": "Solar/Lon123W", "short_name": "Lon123W", "long_name": "Solar/Lon123W",
                    "longitude": "-122.597", "latitude": "45.589", "offset": "-08:12", "offset_min": "-492",
+                   "offset_sec": "-29520", "is_utc": "0"},
+    },
+    {
+        # Portland Int'l Airport PDX: -122.597 longitude, +45.589 latititude, degree-wide tz
+        "params": {"tzname": "Lon123W"},
+        "expect": {"name": "Solar/Lon123W", "short_name": "Lon123W", "long_name": "Solar/Lon123W",
+                   "longitude": "-123", "latitude": "", "offset": "-08:12", "offset_min": "-492",
                    "offset_sec": "-29520", "is_utc": "0"},
     },
     {
@@ -67,6 +95,13 @@ TEST_FIXTURE = [
     },
     {
         # date line -180 degrees longitude, hour-wide tz
+        "params": {"tzname": "East12"},
+        "expect": {"name": "Solar/East12", "short_name": "East12", "long_name": "Solar/East12",
+                   "longitude": "180", "latitude": "", "offset": "+12:00", "offset_min": "720",
+                   "offset_sec": "43200", "is_utc": "0"},
+    },
+    {
+        # date line -180 degrees longitude, hour-wide tz
         "params": {"longitude": -180, "use_lon_tz": False},
         "expect": {"name": "Solar/East12", "short_name": "East12", "long_name": "Solar/East12",
                    "longitude": "-180", "latitude": "", "offset": "+12:00", "offset_min": "720",
@@ -77,6 +112,13 @@ TEST_FIXTURE = [
         "params": {"longitude": -180, "use_lon_tz": True},
         "expect": {"name": "Solar/Lon180E", "short_name": "Lon180E", "long_name": "Solar/Lon180E",
                    "longitude": "-180", "latitude": "", "offset": "+12:00", "offset_min": "720",
+                   "offset_sec": "43200", "is_utc": "0"},
+    },
+    {
+        # date line -180 degrees longitude, degree-wide tz
+        "params": {"tzname": "Lon180E"},
+        "expect": {"name": "Solar/Lon180E", "short_name": "Lon180E", "long_name": "Solar/Lon180E",
+                   "longitude": "180", "latitude": "", "offset": "+12:00", "offset_min": "720",
                    "offset_sec": "43200", "is_utc": "0"},
     },
 ]
@@ -104,10 +146,16 @@ class TestAccessors(unittest.TestCase, LongitudeUtils):
 
         for fixture in TEST_FIXTURE:
             # generate tests based on TEST_FIXTURE array's parameters and expected results
-            lon_str = fixture["params"]["longitude"]
-            use_lon_tz = fixture["params"]["use_lon_tz"]
-            tz_type = "deg" if use_lon_tz else "hour"
-            base_func_name = f"test_{PROGNUM:03}_{testnum:03}_accessor_lon_{lon_str}_{tz_type}"
+            if "longitude" in fixture["params"] and fixture["params"]["longitude"] is not None:
+                lon_str = fixture["params"]["longitude"]
+                use_lon_tz = fixture["params"]["use_lon_tz"]
+                tz_type = "deg" if use_lon_tz else "hour"
+                base_func_name = f"test_{PROGNUM:03}_{testnum:03}_accessor_lon_{lon_str}_{tz_type}"
+            elif "tzname" in fixture["params"] and fixture["params"]["tzname"] is not None:
+                tzname = fixture["params"]["tzname"]
+                base_func_name = f"test_{PROGNUM:03}_{testnum:03}_accessor_tzname_{tzname}"
+            else:
+                raise ValueError("test fixture should contain one of longitude or tzname field")
             Flags.verbose_print(
                 f"generating test {base_func_name}..."
             )
