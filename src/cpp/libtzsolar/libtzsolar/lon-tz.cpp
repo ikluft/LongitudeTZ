dddd10aaa97c76bbe8efae57bd62c15a90f36995
Ian Kluft <ko6yq.dev+github@gmail.com>
2024-05-23 23:57:52 -0700

    start on C++ CLI

---
+++ src/cpp/libtzsolar/libtzsolar/lon-tz.cpp
@@ -0,0 +1,45 @@
+/*
+ * lon-tz.cpp
+ */
+
+#include "libtzsolar.hpp"
+#include <iostream>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[])
+{
+    po::options_description desc("lon-tz longitude time zones tool usage");
+
+    // define options
+    desc.add_options()
+    ("help", "Display this help message")
+    ("version", "Display the version number")
+    ("tzfile", "Generate timezone database file")
+    ("tzname", po::value<std::string>(), "Select a solar time zone by name")
+    ("longitude", po::value<float>(), "Set the longitude parameter for a solar time zone")
+    ("latitude", po::value<float>(), "iSet the optional latitude parameter for a solar time zone")
+    ("type", po::value<std::string>(), "Set the type of a solar time zone as 'longitude' or 'hour', defaults to hour")
+    ("get", po::value<std::string>(), "Specify field(s) to print from a solar time zone")
+    ;
+
+    // process options
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
+    po::notify(vm);
+
+    // print help
+    if (vm.count("help")){
+        std::cout << desc;
+        return 0;
+    }
+
+    // print version
+    if (vm.count("version")) {
+        // TODO
+    }
+
+    // TODO
+    return 0;
+}
