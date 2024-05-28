4843b59bef57ac4bb386938ea414912dbc45ee60
Ian Kluft <ko6yq.dev+github@gmail.com>
2024-05-27 23:19:12 -0700

    WIP: continue adding outline for command-line argument processing

--- src/cpp/libtzsolar/libtzsolar/lon-tz.cpp
+++ src/cpp/libtzsolar/libtzsolar/lon-tz.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
     ("tzfile", "Generate timezone database file")
     ("tzname", po::value<std::string>(), "Select a solar time zone by name")
     ("longitude", po::value<float>(), "Set the longitude parameter for a solar time zone")
-    ("latitude", po::value<float>(), "iSet the optional latitude parameter for a solar time zone")
+    ("latitude", po::value<float>(), "Set the optional latitude parameter for a solar time zone")
     ("type", po::value<std::string>(), "Set the type of a solar time zone as 'longitude' or 'hour', defaults to hour")
     ("get", po::value<std::string>(), "Specify field(s) to print from a solar time zone")
     ;
@@ -32,7 +32,7 @@ int main(int argc, char* argv[])
     po::notify(vm);
 
     // print help
-    if (vm.count("help")){
+    if (vm.count("help")) {
         std::cout << desc;
         return 0;
     }
@@ -41,6 +41,32 @@ int main(int argc, char* argv[])
     if (vm.count("version")) {
         std::cout << "Longitude time zones library, C++ implementation version " << lon_tz_version.full;
         return 0;
+    }
+
+    if (vm.count("tzfile")) {
+        if (vm.count("tzname") || vm.count("longitude")) {
+            std::cerr << "Mutually exclusive arguments: --tzfile cannot be combined with --tzname or --longitude";
+            std::cerr << desc;
+            return 1;
+        }
+        // TODO
+        return 0;
+    }
+
+    if (vm.count("tzname")) {
+        if (vm.count("longitude")) {
+            std::cerr << "Mutually exclusive arguments: --tzname cannot be combined with --longitude";
+            std::cerr << desc;
+            return 1;
+        }
+        // TODO
+        return 0;
+    }
+
+    if (!vm.count("longitude")) {
+        std::cerr << "One argument of --tzfile, --tzname or --longitude is required";
+        std::cerr << desc;
+        return 1;
     }
 
     // TODO
