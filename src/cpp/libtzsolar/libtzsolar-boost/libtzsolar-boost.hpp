e142ad37ad0fd72e32f0e30f602151e347f2fdbc
Ian Kluft <ko6yq.dev+github@gmail.com>
2024-06-27 15:22:35 -0700

    reorg non-one-liner methods out of header

--- src/cpp/libtzsolar/libtzsolar-boost/libtzsolar-boost.hpp
+++ src/cpp/libtzsolar/libtzsolar-boost/libtzsolar-boost.hpp
@@ -41,65 +41,47 @@ namespace longitude_tz {
         virtual string_type dst_zone_abbrev() const {
             return std::string(""); // not defined because there is no DST in solar time zones
         }
+
         // for the zone when not in daylight savings (eg: EST)
         virtual string_type std_zone_abbrev() const {
             return const_cast<TZSolar&>(solar_tz).str_short_name();
         }
+
         // for the timezone when in daylight savings (eg: Eastern Daylight Time)
         virtual string_type dst_zone_name() const {
             return std::string(""); // not defined because there is no DST in solar time zones
         }
+
         // for the zone when not in daylight savings (eg: Eastern Standard Time)
         virtual string_type std_zone_name() const {
             return const_cast<TZSolar&>(solar_tz).str_long_name();
         }
+
         // True if zone uses daylight savings adjustments otherwise false
         virtual bool has_dst() const {
             return false;
         }
+
         // Local time that DST starts -- undefined if has_dst is false
         virtual time_type dst_local_start_time(year_type y) const {
             return pt::ptime(); // not defined because there is no DST in solar time zones
         }
+
         // Local time that DST ends -- undefined if has_dst is false
         virtual time_type dst_local_end_time(year_type y) const {
             return pt::ptime(); // not defined because there is no DST in solar time zones
         }
+
         // Base offset from UTC for zone (eg: -07:30:00)
-        virtual time_duration_type base_utc_offset() const {
-            short offset_part_hr = const_cast<TZSolar&>(solar_tz).get_offset_min() / 60;
-            short offset_part_min = const_cast<TZSolar&>(solar_tz).get_offset_min() % 60;
-            return time_duration_type(offset_part_hr, offset_part_min, 0);
-        }
+        virtual time_duration_type base_utc_offset() const;
+
         // Adjustment forward or back made while DST is in effect
         virtual time_duration_type dst_offset() const {
             return time_duration_type(); // not defined because there is no DST in solar time zones
         }
+
         // Returns a POSIX time_zone string for this object
-        virtual string_type to_posix_string() const  {
-            // std offset dst [offset],start[/time],end[/time] - w/o spaces
-            stringstream_type ss;
-            ss.fill('0');
-
-            // std
-            ss << std_zone_abbrev();
-
-            // offset
-            if (base_utc_offset().is_negative()) {
-                // inverting the sign guarantees we get two digits
-                ss << '-' << std::setw(2) << base_utc_offset().invert_sign().hours();
-            } else {
-                ss << '+' << std::setw(2) << base_utc_offset().hours();
-            }
-            if(base_utc_offset().minutes() != 0 || base_utc_offset().seconds() != 0) {
-                ss << ':' << std::setw(2) << base_utc_offset().minutes();
-                if(base_utc_offset().seconds() != 0) {
-                    ss << ':' << std::setw(2) << base_utc_offset().seconds();
-                }
-            }
-            // skip DST offset because solar time zones never have DST
-            return ss.str();
-        }
+        virtual string_type to_posix_string() const;
     };
 
     typedef solar_time_zone_base<char> solar_time_zone;
