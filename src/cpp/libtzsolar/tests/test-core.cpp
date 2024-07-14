95bdd61184ae676289f7155d872b8753b9d28a1a
Ian Kluft <ko6yq.dev+github@gmail.com>
2024-07-13 23:47:58 -0700

    WIP: start on C++ unit tests

---
+++ src/cpp/libtzsolar/tests/test-core.cpp
@@ -0,0 +1,15 @@
+#include "tappp.hpp"
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <cstdlib>
+
+static const int total_tests = 1;
+
+int main(void) {
+    TAP::plan(total_tests);
+
+    // TODO
+
+    return 0;
+}
