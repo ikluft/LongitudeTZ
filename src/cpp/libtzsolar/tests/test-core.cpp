6befe33ddcd6b1598a64a30954f0bde474c25343
Ian Kluft <ko6yq.dev+github@gmail.com>
2024-07-16 17:45:54 -0700

    WIP/minor: add top level test functions

--- src/cpp/libtzsolar/tests/test-core.cpp
+++ src/cpp/libtzsolar/tests/test-core.cpp
@@ -6,10 +6,20 @@
 
 static const int total_tests = 1;
 
+void test_global() {
+    // TODO
+}
+
+void test_polar() {
+    // TODO
+}
+
 int main(void) {
     TAP::plan(total_tests);
 
-    // TODO
+    // run tests
+    test_global();
+    test_polar();
 
     return 0;
 }
